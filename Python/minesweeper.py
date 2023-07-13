360a9162b8afda18f20574848e0ca68e41b931fa
Umair Saeed <umair.saeed@gmail.com>
2023-07-13 14:04:29 -0700

    [Exercism][Python] Minesweeper. Updated based on feedback from exercism mentor.

--- Python/minesweeper.py
+++ Python/minesweeper.py
@@ -8,48 +8,38 @@ def annotate(minefield):
     if not is_minefield_valid(minefield):
         raise ValueError("The board is invalid with current input.")
 
-    result = [row[:] for row in minefield]
-
-    for r in range(0, num_rows):
-        new_row = ''
-        for c in range(0, num_columns):
-            if minefield[r][c] == '*':
-                new_row += '*'
+    result = []
+    for r, row in enumerate(minefield):
+        new_row = ""
+        for c, elem in enumerate(row):
+            if elem == "*":
+                new_row += "*"
             else:
-                neighboring_values = get_neighboring_values(minefield, r, c)
-                neighboring_mines_count = neighboring_values.count('*')
-                if neighboring_mines_count == 0:
-                    new_row += ' '
-                else:
-                    new_row += f'{neighboring_mines_count}'
-        result[r] = new_row
-
+                mine_count = get_neighboring_mine_count(minefield, r, c)
+                new_row += (
+                    str(mine_count)
+                    if mine_count != 0
+                    else " "
+                )
+        result.append(new_row)
     return result
 
 
-def get_neighboring_values(minefield, row, column):
-    num_rows = len(minefield)
-    if row < 0 or row >= num_rows:
-        raise ValueError('Row is out of bounds')
-
-    num_columns = len(minefield[0])
-    if column < 0 or column >= num_columns:
-        raise ValueError('Column is out of bounds')
-
-    neighbor_indices = [(row, column - 1),
-                        (row, column + 1),
-                        (row - 1, column),
-                        (row - 1, column - 1),
-                        (row - 1, column + 1),
-                        (row + 1, column),
-                        (row + 1, column - 1),
-                        (row + 1, column + 1)]
-
-    valid_rows = filter(lambda t: 0 <= t[0] < num_rows, neighbor_indices)
-    valid_indices = filter(lambda t: 0 <= t[1] < num_columns, valid_rows)
-
-    neighboring_values = [minefield[t[0]][t[1]] for t in valid_indices]
-    return neighboring_values
+def get_neighboring_mine_count(minefield, row, column):
+    return len(
+        [
+            1
+            for ir in [-1, 0, 1]
+            for ic in [-1, 0, 1]
+            if not (
+                (ic == 0 and ir == 0)
+                or (row + ir < 0)
+                or (row + ir >= len(minefield))
+                or (column + ic < 0)
+                or (column + ic >= len(minefield[0]))
+            )
+            and minefield[row + ir][column + ic] == "*"
+        ])
 
 
 def is_minefield_valid(minefield):
@@ -57,8 +47,7 @@ def is_minefield_valid(minefield):
     if any(len(row) != num_columns for row in minefield):
         return False
 
-    for row in minefield:
-        if any((e != ' ' and e != '*') for e in row):
-            return False
+    if not all(all((e == ' ' or e == '*') for e in row) for row in minefield):
+        return False
 
     return True
