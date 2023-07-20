b1db0bfb06937a6cc7434f61449afcff7573709d
Umair Saeed <umair.saeed@gmail.com>
2023-07-20 14:35:04 -0700

    [Exercism][Python] Roman Numerals (refactored to be more compact).

--- Python/roman_numerals.py
+++ Python/roman_numerals.py
@@ -1,67 +1,28 @@
 from typing import Tuple
 
 def roman(number: int) -> str:
+    VALUE_TO_SYMBOL_LIST = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I')
+    ]
+
     result = []
+    remainder = number
 
-    while number > 0:
-        if number >= 1000:
-            result.append("M" * (number // 1000))
-            number %= 1000
-
-        elif number >= 900:
-            result.append("CM")
-            number %= 900
-
-        elif number >= 500:
-            result.append("D")
-            number %= 500
-
-        elif number >= 400:
-            result.append("CD")
-            number %= 400
-
-        elif number >= 100:
-            result.append("C" * (number // 100))
-            number %= 100
-
-        elif number >= 90:
-            result.append("XC")
-            number %= 90
-
-        elif number >= 50:
-            result.append("L")
-            number %= 50
-
-        elif number >= 40:
-            result.append("XL")
-            number %= 40
-
-        elif number >= 10:
-            result.append("X" * (number // 10))
-            number %= 10
-
-        elif number >= 9:
-            result.append("IX")
-            number %= 9
-
-        elif number >= 5:
-            result.append("V")
-            number %= 5
-
-        elif number >= 4:
-            result.append("IV")
-            number %= 4
-
-        else:
-            result.append("I" * number)
-            number %= 1
+    for value, symbol in VALUE_TO_SYMBOL_LIST:
+        while remainder >= value:
+            result.append(symbol)
+            remainder -= value
 
     return "".join(result)
-
-
-# def roman(partial_number: int, current: str) -> Tuple[int, str]:
-#     if number
-#     if partial_number > 1000:
-#         partial_number %- 1000
-#         partial_number.append
-print(roman(93))
